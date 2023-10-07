be0cc035569ad009047b70f5d0563175ff1cda9c
Alexander Alfon <87632745+AAlfon@users.noreply.github.com>
2023-10-07 22:22:27 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+
+<template>
+  <main>
+      <TopicList />
+  </main>
+</template>
+
+
+
+<script>
+  import TopicList from './components/TopicList.vue'
+  export default {
+    components: {
+      TopicList
+    }
+  }
+</script>
+<style scoped>
+  .content-container {
+    widows: 100%;
+    height: auto;
+    background-color: azure;
+  }
+</style>
